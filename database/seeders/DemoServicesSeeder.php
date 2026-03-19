9bdd3e4d2aae97871756a71c0c7204c3df7c1eb3
Eneko Galan <enekogalanelorza@gmail.com>
2026-03-19 19:09:26 +0100

    Add public URLs to demo services in DemoServicesSeeder

--- database/seeders/DemoServicesSeeder.php
+++ database/seeders/DemoServicesSeeder.php
@@ -11,16 +11,19 @@ class DemoServicesSeeder extends Seeder {
             'name' => 'Demo Orders',
             'api_key' => 'demo-service-1-api-key-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
             'base_url' => 'http://demo-app-1:80',
+            'public_url' => 'http://localhost:8081',
         ],
         [
             'name' => 'Demo Notifications',
             'api_key' => 'demo-service-2-api-key-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
             'base_url' => 'http://demo-app-2:80',
+            'public_url' => 'http://localhost:8082',
         ],
         [
             'name' => 'Demo Reports',
             'api_key' => 'demo-service-3-api-key-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
             'base_url' => 'http://demo-app-3:80',
+            'public_url' => 'http://localhost:8083',
         ],
     ];
 
