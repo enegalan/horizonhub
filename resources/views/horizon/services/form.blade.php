9138ea1604383fb61b6963efd90850bbbc8b65e5
Eneko Galan <egalan@oversys.es>
2026-09-10 12:07:16 +0200

    Center tag input layout in Horizon service form

--- resources/views/horizon/services/form.blade.php
+++ resources/views/horizon/services/form.blade.php
@@ -85,7 +85,7 @@
                             </span>
                         </template>
                     </div>
-                    <div class="flex flex-wrap items-end gap-2">
+                    <div class="flex flex-wrap items-center gap-2">
                         <div class="relative min-w-0 flex-1 space-y-2" @click.outside="closeTagSuggestions()">
                             <x-input-label for="service-tag-input">Add tag</x-input-label>
                             <x-text-input
